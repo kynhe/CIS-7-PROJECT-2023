f115a325a0ed0450aad01ee1cdbce478d24e642d
kynhe <125181320+kynhe@users.noreply.github.com>
2023-06-06 01:43:53 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -28,17 +28,17 @@ int main(int argc, char** argv) {
     
     //Map/Process the Inputs -> Outputs
     cout << "Enter the plain text: ";
-    getline(cin, plain);
+    getline(cin, plain); // Read the plain text from the user
 
     cout << "Enter the keyword: ";
-    getline(cin, key);
+    getline(cin, key); // Read the keyword from the user
 
-    string eText = encrypt(plain, key);
-    string dText = decrypt(eText, key);
+    string eText = encrypt(plain, key); // Encrypt the plain text
+    string dText = decrypt(eText, key); // Decrypt the cipher text
 
     //Display Inputs/Outputs
-    cout << "Encrypted text: " << eText << endl;
-    cout << "Decrypted text: " << dText << endl;
+    cout << "Encrypted text: " << eText << endl; // Print the encrypted text
+    cout << "Decrypted text: " << dText << endl; // Print the decrypted text
     
     //Clean up memory and files
     
@@ -46,58 +46,58 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-// Function to encrypt the plain text using the Vigenere cipher
+// Function to encrypt the plain text 
 string encrypt(string plain, string key) {
     string cipher;
     int keyL = key.length();
 
     for (int i = 0; i < plain.length(); ++i) {
-        char pChar = plain[i];
-        char kChar = key[i % keyL];
-        int pOffset = pChar - 'A';
-        int kOffset = kChar - 'A';
-        int eOffset = (pOffset + kOffset) % 26;
-        char eChar = 'A' + eOffset;
+        char pChar = plain[i]; // Get the current character from the plain text
+        char kChar = key[i % keyL]; // Get the current character from the key
+        int pOffset = pChar - 'A'; // Calculate the offset of the plain character
+        int kOffset = kChar - 'A'; // Calculate the offset of the key character
+        int eOffset = (pOffset + kOffset) % 26; // Calculate the offset of the encrypted character
+        char eChar = 'A' + eOffset; // Convert the offset back to the encrypted character
 
         // Handle lowercase letters
         if (islower(pChar)) {
-            eChar = tolower(eChar);
+            eChar = tolower(eChar); // Convert the encrypted character to lowercase
         }
         // Keep non-alphabetic characters unchanged
         else if (!isupper(pChar)) {
-            eChar = pChar;
+            eChar = pChar; // Keep non-alphabetic characters unchanged
         }
 
-        cipher += eChar;
+        cipher += eChar; // Append the encrypted character to the cipher text
     }
 
-    return cipher;
+    return cipher; // Return the encrypted text
 }
 
-// Function to decrypt the cipher text using the Vigenere cipher
+// Function to decrypt the cipher text 
 string decrypt(string cipher, string key) {
     string plain;
     int keyL = key.length();
 
     for (int i = 0; i < cipher.length(); ++i) {
-        char cChar = cipher[i];
-        char kChar = key[i % keyL];
-        int cOffset = cChar - 'A';
-        int kOffset = kChar - 'A';
-        int dOffset = (cOffset - kOffset + 26) % 26;
-        char dChar = 'A' + dOffset;
+        char cChar = cipher[i]; // Get the current character from the cipher text
+        char kChar = key[i % keyL]; // Get the current character from the key
+        int cOffset = cChar - 'A'; // Calculate the offset of the cipher character
+        int kOffset = kChar - 'A'; // Calculate the offset of the key character
+        int dOffset = (cOffset - kOffset + 26) % 26; // Calculate the offset of the decrypted character
+        char dChar = 'A' + dOffset; // Convert the offset back to the decrypted character
 
         // Handle lowercase letters
         if (islower(cChar)) {
-            dChar = tolower(dChar);
+            dChar = tolower(dChar); // Convert the decrypted character to lowercase
         }
         // Keep non-alphabetic characters unchanged
         else if (!isupper(cChar)) {
-            dChar = cChar;
+            dChar = cChar; // Keep non-alphabetic characters unchanged
         }
 
-        plain += dChar;
+        plain += dChar; // Append the decrypted character to the plain text
     }
 
-    return plain;
+    return plain; // Return the decrypted text
 }
